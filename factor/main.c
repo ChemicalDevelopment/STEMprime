a349bf9d37d7fe7ce123ec945eee7a9f0da8a8a3
CadeBrown <brown.cade@gmail.com>
2016-08-21 18:02:43 -0400

    Updated

--- factor/main.c
+++ factor/main.c
@@ -12,26 +12,31 @@ Project: github.chemicaldevelopment/stemprime
 */
 
 int main(int argc, char *argv[]) {
-    int64_t max = 1000000;
-    int64_t start_exp = 10000;
-    int64_t end_exp = 11000;
-    if (argc > 1) start_exp = strtoll(argv[1], NULL, 10);
-    if (argc > 2) end_exp = strtoll(argv[2], NULL, 10);
-    if (argc > 3) max = strtoll(argv[3], NULL, 10);
-    int64_t * to_div = (int64_t *)malloc(sizeof(int64_t) * (max / 64) + 1);
+    int64_t min = 10000;
+    int64_t max = 12000;
+    int64_t max_test = 1000000;
+    if (argc > 1) min = strtoll(argv[1], NULL, 10);
+    if (argc > 2) max = strtoll(argv[2], NULL, 10); 
+    if (argc > 3) max_test = strtoll(argv[3], NULL, 10);
+    if (max_test < max) max_test = max; 
+    int64_t * to_div = (int64_t *)malloc(sizeof(int64_t) * (max_test / 64) + 1);
     int64_t i;
-    for (i = 3; i < max; ++i) {
-        if (i % 2 == 1) {
+    mpz_t n;
+    mpz_init(n);
+    for (i = 3; i < max_test; ++i) {
+        mpz_set_ui(n, i);
+        if (mpz_probab_prime_p(n, 10)) {
             to_div[i / 64] |= 1 << (i % 64);
         }
     }
-    mpz_t n;
-    mpz_init(n);
-    for (i = 2; i < max; ++i) {
-        mpz_set_ui(n, i);
-        printf("%lld : ", (long long) i);
-        test_num(n, to_div);
-        printf("\n");
+    for (i = 32; i < max; ++i) {
+        if (to_div[i / 64] >> (i % 64) & 1 == 1) {
+            mpz_ui_pow_ui(n, 2, i);
+            mpz_sub_ui(n, n, 1);
+            printf("M_%lld : ", (long long) i);
+            test_num(n, to_div);
+            printf("\n");
+        }
     }
 }
 
