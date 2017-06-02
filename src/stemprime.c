e45f157236b44d9a8861b54b16ec85b37ef1281d
Cade Brown <brownce@ornl.gov>
2017-06-02 15:59:24 -0400

    Started using cargs

--- src/stemprime.c
+++ src/stemprime.c
@@ -19,11 +19,32 @@ can also find a copy at http://www.gnu.org/licenses/.
 
 */
 
+#include "cargs.h"
+
 #include "stemprime.h"
 
 
 
 int main(int argc, char *argv[]) {
+    cargs_init(PACKAGE_NAME, VERSION, argc, argv);
+
+    // name, email  this is used for --authors
+    cargs_add_author("Cade Brown", "cade@chemicaldevelopment.us");
+
+    // add two possible keys (the last can be NULL for no second version)
+    cargs_add_arg("", NULL, CARGS_NUM_ANY, CARGS_ARG_TYPE_INT, "exponents");
+    cargs_add_default("-g", "Hello");
+
+    // when the argument is left as "", that means you don't have to add any prefix.
+    // so, if you run `./greet Mars`, the flag "" will have `Mars` in it
+    cargs_add_arg_str("", NULL, "enter your name");
+    cargs_add_default("", "World");
+
+
+    // this parses the arguments
+    cargs_parse();
+
+
     uint32_t exponent = 580673;
     if (argc > 1) exponent = strtol(argv[1], NULL, 10);
     bool res;
